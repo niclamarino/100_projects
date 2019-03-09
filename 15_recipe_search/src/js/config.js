2af7c92fd05cee0458193df707234984b86b1f6b
YOUR NAME <YOUR@EMAIL.com>
2019-03-09 01:37:15 +0100

    Removed files

--- 15_recipe_search/src/js/config.js
+++
@@ -1,3 +0,0 @@
-      const apiKey = 'fbd3c9307c8b61340afc18da06e0ac6e';
-      const apiId = 'd7036247';
-      const cors = 'https://cors-anywhere.herokuapp.com/';
--- 15_recipe_search/src/js/test.js
+++
@@ -1,4 +0,0 @@
-module.exports.addNote = () => {
-    let x = 34;
-    console.log(`I am a template string. Look: ${x}`)
-}
