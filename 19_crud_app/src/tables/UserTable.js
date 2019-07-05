29894a91aadbdcb867ea7c7798a7c44c01e71ccd
YOUR NAME <YOUR@EMAIL.com>
2019-07-05 18:41:49 +0200

    Added files

---
+++ 19_crud_app/src/tables/UserTable.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const UserTable = props => (
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Username</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+    {props.users.length > 0 ? (
+        props.users.map(user => (
+        <tr>
+          <td>{user.name}</td>
+          <td>{user.username}</td>
+          <td>
+            <button onClick={() => {props.editRow(user)}}>Edit</button>
+            <button className="button muted-button" onClick={() => {props.deleteUser(user.id)}}>Delete</button>
+          </td>
+        </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}>No users</td>
+        </tr>     
+      )}
+    </tbody>
+  </table>
+)
+
+export default UserTable
